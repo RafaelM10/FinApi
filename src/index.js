d950c44b0b5bec5232864bddf13db8430a5a4713
meiradev <rafameira10@icloud.com>
2022-09-08 12:34:22 -0300

    feat: add middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found" })
+  }
+
+  request.customer = customer;
+
+  return next()
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -29,16 +43,11 @@ app.post("/account", (request, response) => {
 
 });
 
-app.get("/statement", (request,response) => {
-  const { cpf } = request.headers;
+app.use(verifyIfExistAccountCPF);
 
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).json({ error: "Customer not found" })
-  }
-
+app.get("/statement", verifyIfExistAccountCPF, (request,response) => {
+  const { customer } = request;
   return response.json(customer.statement)
-})
+});
 
 app.listen(3333);
