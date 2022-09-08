8b4077e2701e33fbcd70707b4b40faee525b35f9
meiradev <rafameira10@icloud.com>
2022-09-08 13:31:25 -0300

    feat: add delete account

--- src/index.js
+++ src/index.js
@@ -43,11 +43,86 @@ app.post("/account", (request, response) => {
 
 });
 
-app.use(verifyIfExistAccountCPF);
+//app.use(verifyIfExistAccountCPF);
 
 app.get("/statement", verifyIfExistAccountCPF, (request,response) => {
   const { customer } = request;
   return response.json(customer.statement)
 });
+
+app.post("/deposit",verifyIfExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+  
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Insufficient funds!"});
+  }
+
+  const statementOperation = {
+    amount, 
+    created_at: new Date(),
+    type: "Debit",
+  };
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send();
+})
+
+app.get("/statement/date", verifyIfExistAccountCPF, (request,response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) => statement.created_at.toDateString() === 
+    new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement)
+});
+
+app.put("/account", verifyIfExistAccountCPF,(request,response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
+app.delete("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+})
 
 app.listen(3333);
