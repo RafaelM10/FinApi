97afac6c08079697d5b564385060169512aca3aa
meiradev <rafameira10@icloud.com>
2022-09-08 10:30:59 -0300

    feat: addcreate cpf and not possible create cpf

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
-const { } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  return response.status(201).send();
 
 });
 
